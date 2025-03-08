c142eebe0bf5196329c62e20a4ee7f700771409f
addygit123 <minecraftaddy123@gmail.com>
2025-03-08 11:02:47 +0530

    Fixed empty querySelector error

--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-const lenis = new Lenis()
-
+const lenis = new Lenis();
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+  lenis.raf(time);
+  requestAnimationFrame(raf);
 }
 
-requestAnimationFrame(raf)
-document.querySelector("")
+requestAnimationFrame(raf);
+
 // gsap.registerPlugin(ScrollTrigger)
 gsap.to("img", { x: 100, duration: 2 });
-document.querySelectorAll('.elem').forEach(elem => {
-  let image = elem.querySelector('img')
-  let tl = gsap.timeline()
+document.querySelectorAll(".elem").forEach((elem) => {
+  let image = elem.querySelector("img");
+  let tl = gsap.timeline();
 
   let xTransform = gsap.utils.random(-100, 100);
 
-  tl
-    .set(image, {
-      transformOrigin: `${xTransform <0 ? 0 :'100%'}`,
-    },"start")
-    .to(image, {
-        scale:0,
-        ease:"none",
-        scrollTrigger:{
-          trigger:elem,
-          durartion:3,
-          start:"top top",
-          end:"bottom top",
-          scrub:true
-        }
-    },"start")
-})
+  tl.set(
+    image,
+    {
+      transformOrigin: `${xTransform < 0 ? 0 : "100%"}`,
+    },
+    "start"
+  ).to(
+    image,
+    {
+      scale: 0,
+      ease: "none",
+      scrollTrigger: {
+        trigger: elem,
+        durartion: 3,
+        start: "top top",
+        end: "bottom top",
+        scrub: true,
+      },
+    },
+    "start"
+  );
+});
